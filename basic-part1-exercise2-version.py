acdde100c668bfb51946fd1997be4ad8555024c9
benryan03 <50218096+benryan03@users.noreply.github.com>
2019-05-10 12:12:23 -0400

    Add files via upload

---
+++ basic-part1-exercise2-version.py
@@ -0,0 +1,5 @@
+#https://www.w3resource.com/python-exercises/python-basic-exercises.php
+#2. Write a Python program to get the Python version you are using.
+
+import platform
+print("Installed Python version is: " + platform.python_version())
