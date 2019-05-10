ee482cad8f828b09ca3f706678b45441058e39ed
benryan03 <50218096+benryan03@users.noreply.github.com>
2019-05-10 11:59:55 -0400

    Add files via upload

---
+++ basic-part1-exercise1-twinkle.py
@@ -0,0 +1,9 @@
+#https://www.w3resource.com/python-exercises/python-basic-exercises.php
+#1. Write a Python program to print the following string in a specific format (see the output).
+
+print("Twinkle, twinkle, little star,")
+print("\tHow I wonder what you are!")
+print("\t\tUp above the world so high,")
+print("\t\t\tLike a diamond in the sky.")
+print("Twinkle, twinkle, little star,")
+print("\tHow I wonder what you are")
