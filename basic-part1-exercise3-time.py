f4d39c729d7cc3759deb519005f0a40c0510adcf
benryan03 <50218096+benryan03@users.noreply.github.com>
2019-05-10 12:18:52 -0400

    Add files via upload

---
+++ basic-part1-exercise3-time.py
@@ -0,0 +1,5 @@
+#https://www.w3resource.com/python-exercises/python-basic-exercises.php
+#3. Write a Python program to display the current date and time.
+
+import datetime
+print(datetime.datetime.now())
