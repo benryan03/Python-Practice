7c05b06036c5cc3dc0d99e6d6ac21f883d188e61
benryan03 <50218096+benryan03@users.noreply.github.com>
2019-05-13 16:28:15 -0400

    Add files via upload

---
+++ basic-part1-exercise013-heredoc.py
@@ -0,0 +1,7 @@
+#https://www.w3resource.com/python-exercises/python-basic-exercises.php
+#13. Write a Python program to print the following here document.
+
+print("""a string that you "don't" have to escape
+This
+is a ....... multi-line
+heredoc string --------> example""")
