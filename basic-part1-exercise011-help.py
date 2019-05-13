a78bc3ba809da61b6f6366e0e915ea4f24d27404
benryan03 <50218096+benryan03@users.noreply.github.com>
2019-05-13 12:35:28 -0400

    Add files via upload

---
+++ basic-part1-exercise011-help.py
@@ -0,0 +1,5 @@
+#https://www.w3resource.com/python-exercises/python-basic-exercises.php
+#11. Write a Python program to print the documents (syntax, description etc.) of Python built-in function(s).
+
+x = input("Enter a Python built-in function (abs, len, int, etc: ")
+print(help(x))
