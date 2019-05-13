868191591cc284c8e55cf7f115cbee4963d0f188
benryan03 <50218096+benryan03@users.noreply.github.com>
2019-05-13 11:38:10 -0400

    Add files via upload

---
+++ basic-part1-exercise8-colors.py
@@ -0,0 +1,6 @@
+#https://www.w3resource.com/python-exercises/python-basic-exercises.php
+#7. Write a Python program to display the first and last colors from the following list. 
+
+color_list = ["Red","Green","White" ,"Black"]
+print(color_list[0])
+print(color_list[3])
