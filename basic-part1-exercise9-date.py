71ec2688f373dbc5cc96667fa12cecc5185a3bfc
benryan03 <50218096+benryan03@users.noreply.github.com>
2019-05-13 11:46:16 -0400

    Add files via upload

---
+++ basic-part1-exercise9-date.py
@@ -0,0 +1,6 @@
+#https://www.w3resource.com/python-exercises/python-basic-exercises.php
+#7. Write a Python program to display the examination schedule. (extract the date from exam_st_date).
+
+exam_st_date = (11, 12, 2014)
+
+print("The examination will start from : " + str(exam_st_date[0]) + " / " + str(exam_st_date[1]) + " / " + str(exam_st_date[2]))
