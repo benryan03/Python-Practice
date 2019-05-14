ed1ae08d574fff827d8225caf303f29f45e2555a
benryan03 <50218096+benryan03@users.noreply.github.com>
2019-05-14 10:38:38 -0400

    Add files via upload

---
+++ basic-part1-exercise015-volume.py
@@ -0,0 +1,11 @@
+#https://www.w3resource.com/python-exercises/python-basic-exercises.php
+#15. Write a Python program to get the volume of a sphere with radius 6.
+
+import math
+
+radius = 6
+
+volume = 4/3 * math.pi * radius ** 3
+
+print("Radius: " + str(radius))
+print("Volume: " + str(volume))
