aae473ae6220bd141183be4f74f85bce26f9e4ff
benryan03 <50218096+benryan03@users.noreply.github.com>
2019-05-16 15:07:50 -0400

    Add files via upload

---
+++ basic-part1-exercise043-sysinfo.py
@@ -0,0 +1,5 @@
+#https://www.w3resource.com/python-exercises/python-basic-exercises.php
+#43. Write a Python program to get OS name, platform and release information.
+
+import platform
+print(platform.platform())
