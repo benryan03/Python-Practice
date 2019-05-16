6d29636799e9a7b71ae1d4386154f28036097b5e
benryan03 <50218096+benryan03@users.noreply.github.com>
2019-05-16 11:47:05 -0400

    Add files via upload

---
+++ basic-part1-exercise037-details.py
@@ -0,0 +1,10 @@
+#https://www.w3resource.com/python-exercises/python-basic-exercises.php
+#37. Write a Python program to display your details like name, age, address in three different lines.
+
+name = "Ben"
+age = 26
+address = "Somerville, MA"
+
+print("Name: " + str(name))
+print("Age: " + str(age))
+print("Address: " + str(address))
