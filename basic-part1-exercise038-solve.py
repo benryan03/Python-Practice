13e6135e3e856e38f16629688b3abf93c7e0f42b
benryan03 <50218096+benryan03@users.noreply.github.com>
2019-05-16 11:53:30 -0400

    Add files via upload

---
+++ basic-part1-exercise038-solve.py
@@ -0,0 +1,7 @@
+#https://www.w3resource.com/python-exercises/python-basic-exercises.php
+#38. Write a Python program to solve (x + y) * (x + y).
+
+x = 4
+y = 3
+
+print("Result: " + str((x + y) * (x + y)))
