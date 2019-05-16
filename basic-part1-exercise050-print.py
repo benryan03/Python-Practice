d3e9477942b3b76ed27f02f22cf34995260ad4d1
benryan03 <50218096+benryan03@users.noreply.github.com>
2019-05-16 16:20:10 -0400

    Add files via upload

---
+++ basic-part1-exercise050-print.py
@@ -0,0 +1,5 @@
+#https://www.w3resource.com/python-exercises/python-basic-exercises.php
+#50. Write a Python program to print without newline or space.
+
+for x in range (0, 10):
+	print (x, end ="")
