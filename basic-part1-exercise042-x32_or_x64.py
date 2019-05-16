c0be3060e916367a90a695e2b2e7796d84ad6be5
benryan03 <50218096+benryan03@users.noreply.github.com>
2019-05-16 15:02:16 -0400

    Add files via upload

---
+++ basic-part1-exercise042-x32_or_x64.py
@@ -0,0 +1,5 @@
+#https://www.w3resource.com/python-exercises/python-basic-exercises.php
+#42. Write a Python program to determine if a Python shell is executing in 32bit or 64bit mode on OS.
+
+import platform
+print(platform.architecture())
