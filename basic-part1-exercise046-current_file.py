b4e8f3e27eb832b81e786d0ca4fe579d66a906c6
benryan03 <50218096+benryan03@users.noreply.github.com>
2019-05-16 15:44:36 -0400

    Add files via upload

---
+++ basic-part1-exercise046-current_file.py
@@ -0,0 +1,4 @@
+#https://www.w3resource.com/python-exercises/python-basic-exercises.php
+#46. Write a python program to get the path and name of the file that is currently executing.
+
+print(__file__)
