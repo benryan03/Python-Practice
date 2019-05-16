76720c54073f47fddbfc6f917b0a87087d09f287
benryan03 <50218096+benryan03@users.noreply.github.com>
2019-05-16 15:50:40 -0400

    Add files via upload

---
+++ basic-part1-exercise047-CPUs.py
@@ -0,0 +1,5 @@
+#https://www.w3resource.com/python-exercises/python-basic-exercises.php
+#47. Write a Python program to find out the number of CPUs using.
+
+import multiprocessing
+print(multiprocessing.cpu_count())
