2773e953aadc36f35ec70b7feb90b0207637eecf
benryan03 <50218096+benryan03@users.noreply.github.com>
2019-06-04 23:00:50 -0400

    Add files via upload

---
+++ basic-part1-exercise051-profiling.py
@@ -0,0 +1,6 @@
+#https://www.w3resource.com/python-exercises/python-basic-exercises.php
+#51. Write a Python program to determine profiling of Python programs.
+
+import cProfile
+
+cProfile.run('print("hello world")')
